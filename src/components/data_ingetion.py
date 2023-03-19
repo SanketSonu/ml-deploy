9e1b63dab4475805c3de407100a5f9eec4ba8e84
sanketsonu <sanketsonu3@gmail.com>
2023-03-20 00:11:36 +0530

    adding data_ingestion code

--- src/components/data_ingetion.py
+++
@@ -1,2 +0,0 @@
-# Reading the data from Database, links, file location etc and splitting it into Train and Test files. 
-# All these steps will come under this file. So it is a part of module component.
