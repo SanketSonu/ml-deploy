15b187bf1b8fbca0125be5550d77b404f6c4972f
SanketSonu <sanketsonu321@gmail.com>
2023-04-26 04:50:51 +0530

    updating app

--- application.py
+++ application.py
@@ -22,21 +22,21 @@ def predict_datapoint():
         return render_template('home.html')
     else:
         data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            gender = request.form.get('gender'),
+            race_ethnicity = request.form.get('ethnicity'),
+            parental_level_of_education = request.form.get('parental_level_of_education'),
+            lunch = request.form.get('lunch'),
+            test_preparation_course = request.form.get('test_preparation_course'),
+            reading_score = float(request.form.get('writing_score')),
+            writing_score = float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
 
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=round(results[0], 1))  # Making result score round to 1 digit after decimal
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results = round(results[0], 1))  # Making result score round to 1 digit after decimal
     
 
 if __name__=="__main__":
