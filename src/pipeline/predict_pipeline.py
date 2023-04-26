db75811bb54f2a2c5dcf1001b892268f436c3a22
SanketSonu <sanketsonu321@gmail.com>
2023-04-26 15:48:03 +0530

    updating predict_pipeline file

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,6 @@
 # When model is created and working fine, we can use this file to predict a 'new data'.
 
+import os
 import sys
 import pandas as pd
 from src.exception import CustomException
@@ -12,8 +13,10 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
+            # model_path='artifacts\model.pkl'
+            # preprocessor_path='artifacts\preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
